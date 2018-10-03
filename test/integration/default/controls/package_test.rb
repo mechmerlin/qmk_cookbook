9bf88ef010145bc98dc44e6d99447d5eb4d985fb
mechmerlin <mechmerlin@gmail.com>
2018-10-03 13:18:15 -0700

    test for all packages required for debian

--- test/integration/default/controls/package_test.rb
+++ test/integration/default/controls/package_test.rb
@@ -1,10 +1,14 @@
 title 'packages'
 
+packages = ['build_essential', 'gcc', 'unzip', 'wget', 'zip', 'gcc-avr', 'binutils-avr', 'avr-libc', 'dfu-programmer', 'dfu-util', 'gcc-arm-none-eabi', 'binutils-arm-none-eabi', 'libnewlib-arm-none-eabi', 'git', 'diffutils']
+
 control 'packages' do
   title 'what packages are installed'
   desc 'Verify packages are installed'
 
-  describe package('git') do
-    it { should be_installed }
+  packages.each do |dependency|
+    describe package(dependency) do
+      it { should be_installed }
+    end
   end
 end
