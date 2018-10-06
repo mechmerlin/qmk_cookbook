33deaf75382afe672ba7b0a066d97a837c00f53f
mechmerlin <mechmerlin@gmail.com>
2018-10-06 10:39:31 -0700

    bump the version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mechmerlin@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures qmk_cookbook'
 long_description 'Installs/Configures qmk_cookbook'
-version '0.1.0'
+version '0.2.0'
 chef_version '>= 14.0' if respond_to?(:chef_version)
 
 issues_url 'https://github.com/mechmerlin/qmk_cookbook/issues'
