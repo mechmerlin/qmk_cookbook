7f716a7d23b1c0e7554c232b5709af4d96a7f4c0
mechmerlin <mechmerlin@gmail.com>
2019-03-23 20:03:22 -0700

    use qmk_repo resource in a recipe

--- recipes/firmware.rb
+++ recipes/firmware.rb
@@ -1,34 +1,14 @@
-
 install_dir = node['qmk']['install_location']
 user = node['qmk']['admin_user']
 user_dir = mac_os_x? ? '/Users' : '/home'
 user_home = ::File.join(user_dir, user)
 install_location = ::File.join(user_home, install_dir)
-qmk_fw_dir = ::File.join(install_location, 'qmk_firmware')
 
-directory ::File.join(install_location) do
-  owner user
-  mode '0755'
+qmk_repo 'download qmk_firmware' do
+  github_user node['qmk']['github_user']
+  repo_name 'qmk'
+  install_dir install_location
   action :create
-end
-
-directory qmk_fw_dir do
-  owner user
-  mode '0755'
-  action :create
-end
-
-github_user = if node['qmk']['github_user'].empty?
-                'qmk'
-              else
-                node['qmk']['github_user']
-              end
-
-git 'clone qmk_firmware fork from github' do
-  repository "git://github.com/#{github_user}/qmk_firmware.git"
-  reference 'master'
-  destination qmk_fw_dir
-  action :sync
 end
 
 apt_pkgs = node['qmk']['firmware']['ubuntu_pkgs']
