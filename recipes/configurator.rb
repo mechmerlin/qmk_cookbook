49b2139d7baa15f69b6ffc0efb5993bbd69a1b78
mechmerlin <mechmerlin@gmail.com>
2019-05-31 19:17:48 -0700

    refactor configurator recipe

--- recipes/configurator.rb
+++ recipes/configurator.rb
@@ -1,32 +1,15 @@
-
-install_dir = node['qmk']['install_location']
 user = node['qmk']['admin_user']
 user_dir = mac_os_x? ? '/Users' : '/home'
 user_home = ::File.join(user_dir, user)
-install_location = ::File.join(user_home, install_dir)
-qmk_conf_dir = ::File.join(install_location, 'qmk_configurator')
+install_location = ::File.join user_home, 'Documents'
+qmk_dir = ::File.join install_location, 'qmk_configurator'
 
-directory ::File.join(install_location) do
-  owner user
-  mode '0755'
+directory install_location
+directory qmk_dir
+
+qmk_repo "clone qmk_configurator into #{install_location}" do
+  github_user node['qmk']['github_user']
+  repo_name 'qmk_configurator'
+  install_dir qmk_dir
   action :create
-end
-
-directory qmk_conf_dir do
-  owner user
-  mode '0755'
-  action :create
-end
-
-github_user = if node['qmk']['github_user'].empty?
-                'qmk'
-              else
-                node['qmk']['github_user']
-              end
-
-git 'clone qmk_firmware fork from github' do
-  repository "git://github.com/#{github_user}/qmk_configurator.git"
-  reference 'master'
-  destination qmk_conf_dir
-  action :sync
 end
